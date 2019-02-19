0bc030cdd611c86d9af2c18050107b5aa9339575
Stephane Couvreur <stephane.couvreur.sueron@gmail.com>
2019-02-19 00:44:06 +0000

    CIFAR subfolder structure started in bash, implemented in Python

--- subfolders.sh
+++ subfolders.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-
 declare -a uniq_whale_ids=(`head -n 10 train.csv | cut -d ',' -f 2 | sort | uniq | grep -v Id`)
-
 
 make_whale_id_dirs() {
 	for id in "${uniq_whale_ids[@]}"
 	do
 		echo $id
-		mkdir 
+		mkdir $id
 	done
 }
 
