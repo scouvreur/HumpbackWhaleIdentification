2df6c6cb3e0ac73a829167fc081c85346007a2b3
Stephane Couvreur <stephane.couvreur.sueron@gmail.com>
2019-02-27 02:17:39 +0000

    [Setup][Script] setup.sh now calls code in scripts/ and single function to download the code and make all working data directories to be imported into fastai ImageDataBunch

--- setup.sh
+++ setup.sh
@@ -3,14 +3,17 @@
 kaggle competitions download -c humpback-whale-identification
 
 mkdir data/
-
 mv *.csv data/
 
 unzip_images() {
 	echo $1
-	echo mkdir data/$1 && unzip $1.zip -d data/$1
-	echo rm $1.zip
+	mkdir data/$1 && unzip $1.zip -d data/$1
+	rm $1.zip
 }
 
 unzip_images train
 unzip_images test
+
+python3 scripts/subfolders.py
+sh scripts/labels.sh
+python3 scripts/make_validation_dir.py
